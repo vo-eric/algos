2b66ada5d16de4744944fdb5948226ddbe1349c3
Eric Vo <vo.eric@gmail.com>
2025-06-24 14:22:27 -0400

    Update animations and add more information

--- src/app/sort/quicksort/page.tsx
+++ src/app/sort/quicksort/page.tsx
@@ -12,22 +12,24 @@ DISPLAY THE LEFT AND RIGHT ARRAY AT EACH ITERATION
 
 export default function QuickSort() {
   const [currentIndex, setCurrentIndex] = useState(-1);
-  const nums = [1, 3, 8, 4, 2, -1, 9];
+  const nums = [3, 5, 1, 8, 4, 2, -1, 9];
   // const nums = [5, 3, 8, 4, 2, -1];
   const { snapshots, sorted } = quickSort(nums);
+  const MAX_INDEX = snapshots.length * 2 - 1;
 
   console.log(snapshots);
 
   return (
     <div className="mx-auto w-[60%]">
       <div className="flex items-center">
-        <div className="mx-auto flex flex-col gap-10">
+        <div className="flex w-full flex-col items-center gap-10">
           <h1 className="text-4xl font-bold">Quick Sort</h1>
           <p>Current Index:{currentIndex}</p>
           <div className="flex gap-5 text-red-700">
-            {nums.map((num, i) => (
+            [{nums.join(", ")}]
+            {/* {nums.map((num, i) => (
               <div key={i}>{num}</div>
-            ))}
+            ))} */}
           </div>
           <div className="flex flex-col gap-10">
             {/* need to reverse the snapshots to show the correct order */}
@@ -57,16 +59,16 @@ export default function QuickSort() {
           </div>
         </div>
       </div>
-      <div className="absolute top-[50%] flex translate-y-[-50%] gap-5">
+      <div className="fixed bottom-[5%] left-[50%] flex translate-x-[-50%] gap-5">
         <button
           className="h-[40px] rounded-sm border border-black px-4 py-2"
-          onClick={() => setCurrentIndex(currentIndex - 1)}
+          onClick={() => setCurrentIndex(Math.max(0, currentIndex - 1))}
         >
           Previous
         </button>
         <button
           className="h-[40px] rounded-sm border border-black px-4 py-2"
-          onClick={() => setCurrentIndex(currentIndex + 1)}
+          onClick={() => setCurrentIndex(Math.min(MAX_INDEX, currentIndex + 1))}
         >
           Next
         </button>
