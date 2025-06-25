5ca31a5d2904d644dc64b08100da69499668a4f0
Eric Vo <vo.eric@gmail.com>
2025-06-25 14:47:44 -0400

    Change folder structure

--- src/app/search/dfs/Snapshot.tsx
+++ src/app/search/dfs/Snapshot.tsx
@@ -9,29 +9,27 @@ export default function Snapshot({
 }) {
   return (
     <div
-      className={`grid grid-cols-${snapshot[0].length} grid-rows-${snapshot.length} gap-4`}
+      className={`grid grid-cols-${snapshot[0].length} grid-rows-${snapshot.length}`}
     >
       {snapshot.map((row, i) => {
         return (
           // <div key={i}>
-          <div key={i} className="flex gap-2">
+          <div key={i} className="flex">
             {row.map((cell, j) => {
               return (
                 <div
                   className={clsx(
-                    "flex h-10 w-10 items-center justify-center border border-gray-300",
+                    "flex h-30 w-30 items-center justify-center border border-gray-300 bg-cover bg-center bg-no-repeat",
                     {
-                      "bg-blue-500": cell === "1",
+                      "bg-[url('/land.jpeg')]": cell === "1",
                     },
                     {
-                      "border-red-800":
+                      "bg-[url('/tidal-wave.jpeg')]":
                         i === currentCell?.[0] && j === currentCell?.[1],
                     },
                   )}
                   key={`${i}-${j}`}
-                >
-                  {cell}
-                </div>
+                ></div>
               );
             })}
           </div>
