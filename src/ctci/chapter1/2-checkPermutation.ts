afb8d57ac822693163f2d0edf0a3970b4f3c8d35
Eric Vo <vo.eric@gmail.com>
2025-09-15 12:40:29 -0400

    add 1.2

---
+++ src/ctci/chapter1/2-checkPermutation.ts
@@ -0,0 +1,30 @@
+/**
+ * Check Permutation:
+ *
+ * Given two strings, write a method to decide if
+ * one is a permutation of the other.
+ */
+
+const checkPermutation = (s1: string, s2: string): boolean => {
+  const s1Map = new Map();
+
+  for (const char of s1) {
+    s1Map.set(char, (s1Map.get(char) ?? 0) + 1);
+  }
+
+  for (const char of s2) {
+    if (!s1Map.has(char)) {
+      return false;
+    }
+
+    s1Map.set(char, s1Map.get(char) - 1);
+
+    if (s1Map.get(char) === 0) {
+      s1Map.delete(char);
+    }
+  }
+
+  return s1Map.size === 0;
+};
+
+console.log(checkPermutation("abcdef", "abefcd"));
